dff0361140a2a3c3620dc9425b0600a589a9a36c
kiranbabup <kiran12146@gmail.com>
2023-12-22 12:21:19 +0530

    googlebutton modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { SnackbarProvider } from "./components/SnackBar";
 import Router from "./customRoutes";
 import ThemeProvider from "./theme";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+const clientId = "136010808221-qcqe91l44c3i8060ib6novlgnmjkc8ot.apps.googleusercontent.com";
 
 function App() {
   return (
     <ThemeProvider>
+      <GoogleOAuthProvider clientId={clientId}>
         <SnackbarProvider>
           <Router />
         </SnackbarProvider>
+        </GoogleOAuthProvider>
     </ThemeProvider>
   );
 }
